70384736a765b062087695520341c63ee36730da
XuPenglei <xupenglei87@163.com>
2020-04-27 07:56:26 +0800

    图像缩放

---
+++ DataZoom.py
@@ -0,0 +1,10 @@
+'''
+@Author: your name
+@Date: 2020-04-27 07:54:44
+@LastEditTime: 2020-04-27 07:55:46
+@LastEditors: Please set LastEditors
+@Description: In User Settings Edit
+@FilePath: \BuildMultiResolutionDataset\DataZoom.py
+'''
+import cv2
+import numpy as np 
